5f024da6658c59754895012eab2b39fade0cc85c
Izsán Luca <izsan.luca04@gmail.com>
2021-03-15 00:25:30 +0100

    add base file structure

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let elet = 0;
 
-function az()
+function bruh()
 {
     elet++;
     document.getElementById("vilag").innerHTML = elet;
@@ -28,4 +28,62 @@ function az()
     {
         document.getElementById("vilag").innerHTML += "<br> 📓 poetry 📓";
     }
+    else if(elet == 73)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 📢 yas queen 📢";
+    }
+    else if(elet == 81)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🔮 witchcraft 🔮";
+    }
+    else if(elet == 96)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 💊 happy pills 💊 ";
+    }
+    else if(elet == 104)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 👉👈 ";
+    }
+    else if(elet == 136)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🐸 frOgs 🐸";
+    }
+    else if(elet == 267)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🧃 fruit juice 🧃";
+    }
+    else if(elet == 342)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🔊 bruh 🔊";
+    }
+    else if(elet == 486)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 💅 periodt 💅";
+    }
+    else if(elet == 562)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 📈 it do be like that sometimes 📉";
+    }
+    else if(elet == 674)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🌈 rainbow 🌈";
+    }
+    else if(elet == 732)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🐧 penguins 🐧";
+    }
+    else if(elet == 846)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🥤 energy drinks 🥤";
+    }
+    else if(elet == 934)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🧚‍♀️ lolz 🧚‍♀️";
+    }
+    else if(elet == 1000)
+    {
+        document.getElementById("vilag").innerHTML += "<br> final boss.. kind of 👀";
+        document.getElementById("vilag").innerHTML += "<br><a href='https://www.youtube.com/watch?v=5RIcPBBCpzY'>here</a>";
+    }
+
 }

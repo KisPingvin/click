fb8107cb7a37c5889db1b183f2fee6a63a2fc483
Izsán Luca <izsan.luca04@gmail.com>
2021-03-14 22:43:58 +0100

    add base file structure

---
+++ index.js
@@ -0,0 +1,31 @@
+let elet = 0;
+
+function az()
+{
+    elet++;
+    document.getElementById("vilag").innerHTML = elet;
+    if(elet == 69)
+    {
+        document.getElementById("vilag").innerHTML += "<br> ✨ u're gay ✨";
+    }
+    else if(elet == 24)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🔴 girl in red? 🔴";
+    }
+    else if(elet == 43)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🦕 u're cute 🦕";
+    }
+    else if(elet == 16)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🧊☕ ice cofy ☕🧊 ";
+    }
+    else if(elet == 37)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 🍄 mushrooms 🍄";
+    }
+    else if(elet == 51)
+    {
+        document.getElementById("vilag").innerHTML += "<br> 📓 poetry 📓";
+    }
+}
